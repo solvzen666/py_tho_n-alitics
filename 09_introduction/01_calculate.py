9a2bf00dcb7334501a06f296d3db79cc8a11180f
solvzen666 <88481037+solvzen666@users.noreply.github.com>
2022-11-28 17:16:37 +0300

    Add files via upload

---
+++ 09_introduction/01_calculate.py
@@ -0,0 +1,12 @@
+# Вычислить
+# 1. 9**2+6**4
+# 2. math.sqrt(8)-math.sqrt(12)
+# 3. math.sin(45)+math.cos(45)
+# 4. math.log(54)
+
+print( "9**2+6**4 =", 9**2+6**4)
+
+import math
+print( "math.sqrt(8)-math.sqrt(12) =", math.sqrt(8)-math.sqrt(12))
+print( "math.sin(45)+math.cos(45) =", math.sin(45)+math.cos(45))
+print("math.log(54) =", math.log(54))
